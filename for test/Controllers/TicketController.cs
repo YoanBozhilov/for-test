41d3850b62252ddaf6c083ccdaaa427e7037b2c5
YoanBozhilov <yoanbbozhilov@gmail.com>
2023-04-30 15:05:16 +0300

    ticket service and controller

---
+++ for test/Controllers/TicketController.cs
@@ -0,0 +1,80 @@
+﻿using for_test.DTOs;
+using for_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using for_test.Services;
+
+namespace for_test.Controllers
+{
+    public class TicketController : Controller
+    {
+        private TicketService ticketService;
+
+        public TicketController(TicketService ticketService)
+        {
+            this.ticketService = ticketService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<TicketDTO> tickets = ticketService.GetAll();
+
+            return View(tickets);
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Ticket ticket = ticketService.GetById(id);
+
+            return View(ticket);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Ticket ticket)
+        {
+            ticketService.Create(ticket);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Ticket ticket = ticketService.GetById(id);
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Ticket ticket)
+        {
+            ticketService.Edit(ticket);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Ticket ticket = ticketService.GetById(id);
+
+            return View(ticket);
+        }
+
+        [HttpPost]
+        public IActionResult DeleteConfirm(int id)
+        {
+            ticketService.Delete(id);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+
---
+++ for test/Services/TicketService.cs
@@ -0,0 +1,58 @@
+﻿using for_test.DTOs;
+using for_test.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace for_test.Services
+{
+    public class TicketService
+    {
+        private SQLDbContext dbContext;
+
+        public TicketService(SQLDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public List<TicketDTO> GetAll()
+        {
+            return dbContext.Tickets.Select(u => ToDTO(u)).ToList();
+        }
+
+        public Ticket GetById(int id)
+        {
+            return dbContext.Tickets.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Create(Ticket ticket)
+        {
+            dbContext.Tickets.Add(ticket);
+            dbContext.SaveChanges();
+        }
+
+        public void Edit(Ticket ticket)
+        {
+            Ticket dbTicket = GetById(ticket.Id);
+
+            dbTicket.Name = ticket.Name;
+            dbTicket.Price = ticket.Price;
+
+            dbContext.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Ticket dbTicket = GetById(id);
+
+            dbContext.Tickets.Remove(dbTicket);
+            dbContext.SaveChanges();
+        }
+        private static TicketDTO ToDTO(Ticket ticket)
+        {
+            return new TicketDTO(ticket.Id, ticket.Name, ticket.Price);
+        }
+    }
+}
